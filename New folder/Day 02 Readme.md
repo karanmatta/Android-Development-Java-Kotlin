a0f66f9fb50edad8052ef0e3becd75f06f816946
karanmatta <karanmatta28@gmail.com>
2022-11-12 00:05:02 +0530

    Day 02 Show My name Application with Button

---
+++ New folder/Day 02 Readme.md
@@ -0,0 +1,10 @@
+Day 02
+I learned about the following:
+
+How to add Buttons to the UI
+How to add a label to the UI
+How to add a text field to the UI
+How to add a text view to the UI
+I learned about constraints in the UI
+
+
---
+++ New folder/ShowMyName/app/src/main/java/com/bawp/showmyname/MainActivity.java
@@ -0,0 +1,35 @@
+package com.bawp.showmyname;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button showButton;
+    private TextView nameText;
+    private EditText enterName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showButton = findViewById(R.id.button);
+        nameText = findViewById(R.id.textView);
+        enterName = findViewById(R.id.editTextName);
+
+        showButton.setOnClickListener(view -> {
+            String name = enterName.getText().toString();
+            if (name.isEmpty()) {
+                nameText.setText("Hello, Bond!");
+            }else {
+                nameText.setText("Hello, " + name);
+            }
+
+        });
+    }
+}
